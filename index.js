31a264e499b85c2f90d55ec5e2ad742d1f6ebbce
KryptomSilver <abelromeror763@gmail.com>
2023-11-27 13:50:43 -0600

    finish app

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const { Server } = require("socket.io");
 const http = require("http");
 const app = express();
-
 const server = http.createServer(app);
+const io = new Server(server);
 
+app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
-  res.send("<h1>INICIO DE SERVER</h1>");
+  res.sendFile(__dirname + "/src/index.html");
 });
 
+io.on("connection", (socket) => {
+  socket.on("send-message", function (message, nickname, color) {
+    io.emit("send-message", message, nickname, color);
+    //console.log(`message from ${nickname}: ${message}`);
+  });
+});
 server.listen(3000, () => {
-  console.info("Server is running on PORT 300");
+  console.info("Server is running on PORT 3000");
 });
